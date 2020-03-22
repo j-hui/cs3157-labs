20918787b43efb0844dd3f6e270d9767c2abd30a
Kevin Chen <kevin@kevinchen.co>
2020-03-22 11:28:58 -0700

    lab9: Add MyString solution.

---
+++ lab9/mystring.h
@@ -0,0 +1,15 @@
+/*
+ * mystring.h
+ */
+
+#ifndef __MYSTRING_H__
+#define __MYSTRING_H__
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef std::string MyString;
+
+#endif
